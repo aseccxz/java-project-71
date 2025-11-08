069a4b78ce797bd9b957715b4f283700246d6ae0
aseccxz <aseccxz@gmail.com>
2025-11-08 16:43:57 +0300

    reading and parsing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("info.picocli:picocli:4.7.7")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.20.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,11 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 // some exports omitted for the sake of brevity
 
 @Command(name = "gendiff", version = "App 1.0", mixinStandardHelpOptions = true,
@@ -11,17 +16,33 @@ import picocli.CommandLine.Parameters;
 
 public class App implements Runnable {
     //@Option(names = "-c", description = "create a new archive")
-    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    /*@Parameters(paramLabel = "filepath1", description = "path to first file")
     String path1;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
     String path2;
     @Option(names = { "-f", "--file" }, paramLabel = "format", description = "output format [default: stylish]")
-
+*/
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
     @Override
     public void run() {
+        try {
+            fileParse();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void fileParse() throws Exception {
+        Path pathToFile1 = Paths.get("/home/aseccxz/projects/secondProject/java-project-71/app/src/main/java/hexlet/code/file1.json");
+        Path pathToFile2 = Paths.get("/home/aseccxz/projects/secondProject/java-project-71/app/src/main/java/hexlet/code/file2.json");
+        String jsonContent1 = Files.readString(pathToFile1);
+        String jsonContent2 = Files.readString(pathToFile2);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Integer> scoreByName1 = mapper.readValue(jsonContent1, Map.class);
+        Map<String, Integer> scoreByName2 = mapper.readValue(jsonContent2, Map.class);
+        System.out.println("File1: " + scoreByName1);
+        System.out.println("File2: " + scoreByName2);
     }
 }
