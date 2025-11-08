5041ff04b77b1c558a692fe8bee0adb0e3c1c6b7
aseccxz <aseccxz@gmail.com>
2025-11-08 15:18:11 +0300

    extended picocli stuff

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,27 +7,21 @@ import picocli.CommandLine.Parameters;
 // some exports omitted for the sake of brevity
 
 @Command(name = "gendiff", version = "App 1.0", mixinStandardHelpOptions = true,
-    description = "Compares two configuration files and shows a difference.")
+        description = "Compares two configuration files and shows a difference.")
+
 public class App implements Runnable {
-
-    /*@Option(names = { "-s", "--font-size" }, description = "Font size")
-    int fontSize = 19;
-
-    @Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli",
-            description = "Words to be translated into ASCII art.")
-    private String[] words = { "Hello,", "picocli" };
-*/
-    @Override
-    public void run() {
-        // The business logic of the command goes here...
-        // In this case, code for generation of ASCII art graphics
-        // (omitted for the sake of brevity).
-        System.out.println("Hi, world");
-    }
+    //@Option(names = "-c", description = "create a new archive")
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    String path1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    String path2;
+    @Option(names = { "-f", "--file" }, paramLabel = "format", description = "output format [default: stylish]")
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
-
+    }
+    @Override
+    public void run() {
     }
 }
