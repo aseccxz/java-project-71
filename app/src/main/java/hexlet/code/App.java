2f899489c3d7f2367e777e6a438437c1320e34fa
aseccxz <aseccxz@gmail.com>
2025-11-10 21:10:23 +0300

    Ckestyle fixes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,32 +1,27 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import org.apache.commons.collections4.CollectionUtils;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
+//import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", version = "App 1.0", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Runnable {
+public final class App implements Runnable {
     //@Option(names = "-c", description = "create a new archive")
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    String path1;
+    private String path1;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    String path2;
-   /* @Option(names = { "-f", "--file" }, paramLabel = "format", description = "output format [default: stylish]")
-*/
+    private String path2;
+   //@Option(names = { "-f", "--file" }, paramLabel = "format", description = "output format [default: stylish]")
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
@@ -65,7 +60,7 @@ public class App implements Runnable {
 
     }
 
-    public Map <String, String> getMapFromJSON (Path path) throws Exception {
+    public static Map<String, String> getMapFromJSON(Path path) throws Exception {
         String jsonContent = Files.readString(path);
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(jsonContent, new TypeReference<Map<String, String>>() { });
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class Differ {
-    public static String generate(Map<String, String> json1, Map <String, String> json2) {
+    public static String generate(Map<String, String> json1, Map<String, String> json2) {
         Set<String> keysJson1 = json1.keySet();
         Set<String> keysJson2 = json2.keySet();
         List<String> allKeys = (List<String>) CollectionUtils.union(keysJson1, keysJson2);
@@ -16,7 +16,7 @@ public class Differ {
         List<String> uniqueKeysJson2 = (List<String>) CollectionUtils.subtract(keysJson2, keysJson1);
         StringBuilder diff = new StringBuilder();
         allKeys.sort(String::compareTo);
-        allKeys.forEach(key ->{
+        allKeys.forEach(key -> {
             if (commonKeys.contains(key)) {
                 if (json1.get(key).equals(json2.get(key))) {
                     diff.append("  ").append(key).append(": ").append(json1.get(key)).append("\n");
