ec6d8e44250a05eab50877dbf2bfd9317c3400f3
aseccxz <aseccxz@gmail.com>
2025-11-18 21:17:49 +0300

    separated parser and differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,23 +1,37 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
 
 public class Differ {
-    /*public static String getFileContent(){
+    public static String getFileContent(String path) throws IOException {
+        Path pathToFile = Paths.get(path).toAbsolutePath().normalize();
+        return Files.readString(pathToFile);
+    }
+
+    public static ObjectMapper getMapper(String path) {
         String[] pathParts = path.split("\\.");
         String format = pathParts[pathParts.length - 1];
-        Path pathToFile = Paths.get(path).toAbsolutePath().normalize();
-        String content = Files.readString(pathToFile);
-    }*/
+        return switch (format) {
+            case "json" -> new ObjectMapper();
+            case "yml" -> new YAMLMapper();
+            default -> throw new IllegalStateException("Unexpected format: " + format);
+        };
+    }
 
     public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
     }
-    public static String generate(String pathJson1, String pathJson2, String format) throws Exception {
-        Map<String, Object> file1 = Parser.getMapFromJSONPath(pathJson1);
-        Map<String, Object> file2 = Parser.getMapFromJSONPath(pathJson2);
+    public static String generate(String path1, String path2, String format) throws Exception {
+        Map<String, Object> file1 = Parser.getMapFromFile(getFileContent(path1), getMapper(path1));
+        Map<String, Object> file2 = Parser.getMapFromFile(getFileContent(path2), getMapper(path2));
         List<DifferStatusData> changesLog = DifferCalculator.getListOfDifferences(file1, file2);
         return Formatter.formatOption(format, changesLog);
     }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,24 +2,11 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> getMapFromJSONPath(String path) throws Exception {
-        String[] pathParts = path.split("\\.");
-        String format = pathParts[pathParts.length - 1];
-        Path pathToFile = Paths.get(path).toAbsolutePath().normalize();
-        String content = Files.readString(pathToFile);
-        ObjectMapper mapper = switch (format) {
-            case "json" -> new ObjectMapper();
-            case "yml" -> new YAMLMapper();
-            default -> throw new IllegalStateException("Unexpected format: " + format);
-        };
+    public static Map<String, Object> getMapFromFile(String content, ObjectMapper mapper) throws Exception {
         return mapper.readValue(content, new TypeReference<Map<String, Object>>() { });
     }
 }
